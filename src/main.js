b583ea5028191a4c6bae7e27b832ffbf78dbd5bc
hectorcelis <hector.celis@mail.udp.cl>
2026-09-20 01:43:38 +0000

    Agrega validación en consola para evitar selección de equipos iguales

--- src/main.js
+++ src/main.js
@@ -8,7 +8,16 @@ formularios.forEach(form => {
     const equipo1 = inputEquipo1.value;
     const equipo2 = inputEquipo2.value;
 
+  
+  if(equipo1 == equipo2 && != ""){
+   console.error(" error usted no puede jugar contra su mismo equipo")
+   return;
+  }
+
+    console.log("partido valido")
     console.log("Equipo 1 Elegido", equipo1);
-    console.log("Equipo 2 Elegido", equipo2);
+    console.log("Equipo 2 Elegido", equipo2);  
+  
+  
   });
 });
