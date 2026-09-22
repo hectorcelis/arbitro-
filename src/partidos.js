da4f76c0f0c66193d6e6498f099fdcd164a6d49c
hectorcelis <hector.celis@mail.udp.cl>
2026-09-22 01:51:25 +0000

    guarda los cambios hechos

--- src/partidos.js
+++ src/partidos.js
@@ -1,43 +1,17 @@
-const nombrelocal = localStorage.getItem("Equipo Local");
-const nombrevisitante = localStorage.getItem("Equipo Visitante");
+const btnTerminar = document.querySelector( "#btn-terminar");
+btnTerminar.addEventListener("click", () => {
+const golesEq1 = document.querySelector("#goles-eq1").value || 0;
+const golesEq2 = document.querySelector("#goles-eq2").value || 0;
 
-const titulolocal = document.querySelector("#nombre-local");
-const titulovisitante = document.querySelector("#nombre-visita");
-      if( nombrelocal && nombrevisitante){
-        titulolocal.textContent = nombrelocal;
-        titulovisitante.textContent = nombrevisitante;
-      }
-      else{
-        alert("No hay equipos seleccionados volviendo al inicio");
-        window.location.href = "index.html";
-      }
+const amarillasEq1 = document.querySelector("#amarillas-eq1").value || 0;
+const amarillasEq2 = document.querySelector("#amarillas-eq2").value || 0;
 
-        let contadorLocal = 0;
-        let contadorVisita = 0;
-
-        const MarcadorGolesLocal = document.querySelector("#goles-local");
-        const BotonGolLocal = document.querySelector("#btn-gol-local");
-
-         const MarcadorGolesVisitante = document.querySelector("#goles-visita");
-         const BotonGolVisitante = document.querySelector("#btn-gol-visita");
-         
-        BotonGolLocal.addEventListener("click", () =>{
-        contadorLocal++;
-        MarcadorGolesLocal.textContent = contadorLocal;
-        });
-         
-          
-        BotonGolVisitante.addEventListener("click", () =>{
-        contadorVisita++;
-        MarcadorGolesVisitante.textContent = contadorVisita;
-        
-         const botonTerminar = document.querySelector("#btn-terminar");
-  botonTerminar.addEventListener("click", () => {
-    const confirmar = confirm("¿Estás seguro de dar por terminado el partido?");
-     if (confirmar) {
-      localStorage.clear();
-      window.location.href = "index.html";
-      }
+const rojasEq1 = document.querySelector("#rojas-eq1").value || 0;
+const rojasEq2 = document.querySelector("#rojas-eq2").value || 0;
+const incidencias = document.querySelector("#incidencias").value;
+const mensaje = `¿Guardar acta?\n\nEquipo 1: ${golesEq1}\nEquipo 2: ${golesEq2}\nIncidencias: ${incidencias || 'Ninguna'}`;
+if(confirm(mensaje)){
+  localStorage.clear();
+  window.location.replace("./index.html");
+}
 });
-      
-      });
